fcddf1afb68d55901f9649398f91c544bce27be3
theresanother1 <71547484+theresanother1@users.noreply.github.com>
2022-04-24 13:45:55 +0200

    push test theresa

--- app/src/main/java/com/example/studybuddy/BaseApplication.kt
+++ app/src/main/java/com/example/studybuddy/BaseApplication.kt
@@ -2,6 +2,8 @@ package com.example.studybuddy
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+
+
 //Needed for Dependency injection with Hilt!
 @HiltAndroidApp
 class BaseApplication : Application(){
--- app/src/main/java/com/example/studybuddy/screens/ViewStudyGroupScreen.kt
+++ app/src/main/java/com/example/studybuddy/screens/ViewStudyGroupScreen.kt
@@ -1,9 +1,13 @@
 package com.example.studybuddy.screens
 
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 
+@Preview
 @Composable()
-fun ViewStudyGroupScreen(navController: NavHostController){
-
+fun ViewStudyGroupScreen(navController: NavHostController = rememberNavController()){
+    Text(text = "Test")
 }
